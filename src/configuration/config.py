3bb93baf3d20fbb7e8eb6bc6ee563b357a0a994d
Matt <mattperreault.chew@gmail.com>
2021-05-28 16:36:22 -0600

    Update project directory name for better standard.

---
+++ src/configuration/config.py
@@ -0,0 +1,22 @@
+from configparser import ConfigParser
+
+
+def config(config='database.ini', section='postgres') -> dict:
+    parser = ConfigParser()
+
+    parser.read(config)
+
+    db_creds = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            db_creds[param[0]] = param[1]
+
+    else:
+        raise Exception(f'Section {section} not found in the {config} file.')
+
+    return db_creds
+
+
+if __name__ == '__main__':
+    print(config())
---
+++ src/database/PostgresClient.py
@@ -0,0 +1,32 @@
+import psycopg2
+from src.configuration.config import config
+
+
+def initialize_db():
+    """Open a connection to the postgres db"""
+    conn = None
+    try:
+        db_creds = config.config()
+
+        print('Connecting to the db...')
+        conn = psycopg2.connect(**db_creds)
+        cur = conn.cursor()
+
+        print('postgres db version')
+        cur.execute('SELECT version()')
+
+        db_version = cur.fetchone()
+        print(db_version)
+
+        cur.close()
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed.')
+
+
+if __name__ == '__main__':
+    initialize_db()
