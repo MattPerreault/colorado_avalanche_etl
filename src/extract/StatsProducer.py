a9043f06ee91a6253823f2871a3d0da4bce73913
Matt <mattperreault.chew@gmail.com>
2021-05-31 11:20:12 -0600

    Reorg StatProducer to take an endpoint name. Create test dir.

--- src/extract/StatsProducer.py
+++ src/extract/StatsProducer.py
@@ -1,7 +1,14 @@
 import requests
 
 
-AVS_TEAM_ENDPOINT = 'https://statsapi.web.nhl.com/api/v1/teams/21'
+AVS_TEAM = {'endpoint': 'https://statsapi.web.nhl.com/api/v1/teams/21',
+            'endpoint_name': 'team'}
+
+AVS_TEAM_STATS = {'endpoint': 'https://statsapi.web.nhl.com/api/v1/teams/21/stats',
+                  'endpoint_name': 'team stats'}
+
+ENDPOINT_NAMES = ['team', 'team stats']
+ENDPOINT_LIST = [AVS_TEAM, AVS_TEAM_STATS]
 
 
 class StatsProducer:
@@ -9,17 +16,30 @@ class StatsProducer:
     in the NHL API documentation github (see README.md) and returns that
     data as a formatted list of dictionaries.
     """
-    def __init__(self):
+    def __init__(self, endpoint_name=AVS_TEAM['endpoint_name']):
+        assert endpoint_name in ENDPOINT_NAMES, "Endpoint name not defined."
+        self.endpoint = self._get_endpoint_url(endpoint_name)
+
         try:
-            self.raw_data = requests.get(AVS_TEAM_ENDPOINT)
+            self.raw_data = requests.get(self.endpoint)
             self.raw_data.raise_for_status()
         except requests.exceptions.HTTPError as err:
             raise SystemExit(err)
 
-        self.raw_team_data = self.raw_data.json()['teams'][0]
+    def _get_endpoint_url(self, endpoint_name) -> str:
+        """Returns the REST endpoint url for the given name."""
+        ep_url = None
+        for ep_dict in ENDPOINT_LIST:
+            if ep_dict['endpoint_name'] == endpoint_name:
+                ep_url = ep_dict['endpoint']
+        return ep_url
+
+    def _get_raw_data(self) -> dict:
+        """Returns a JSON encoded dict list of raw data."""
+        return self.raw_data.json()
 
     def get_team_data(self) -> dict:
-        raw_team_dict = self.raw_team_data
+        raw_team_dict = self._get_raw_data()['teams'][0]
 
         formatted_team_dict = {
             'id': raw_team_dict['id'],
@@ -37,3 +57,7 @@ class StatsProducer:
         }
 
         return formatted_team_dict
+
+    def get_team_stat_data(self) -> dict:
+        raw_team_dict = self._get_raw_data()
+        return raw_team_dict
